88facb8576321e7621c30870a3ee6b60e0526b0a
Giovani Rodrigues <giovani.srodrigues@hotmail.com>
2022-01-31 22:34:43 -0300

    arquivos de configuracao

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,24 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          //configura o path da pasta root do projeto
+          root: ['./src'],
+          extends: ['.ts', '.tsx', '.js', '.json'],
+          //configura as extensoes que serao usadas
+          alias: {
+            //mapeamentos das pastas do src
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@assets': './src/assets',
+          }
+        }
+      ]
+    ]
   };
 };
